17a92be0a99aea0cae876ea3499b8448c12df534
hero-pixel <happynews1985@yahoo.co.jp>
2020-09-20 22:19:24 +0900

    comit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+<<<<<<< HEAD
   validates :name, presence: true, uniqueness: true
   
+=======
+  has_many :group_users
+  has_many :groups, through: :group_users
+  has_many :messages
+>>>>>>> b92a31d... Message function
 end
